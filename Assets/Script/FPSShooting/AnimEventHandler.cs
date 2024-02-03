1e34c64d4b1b85d63854d61a210165892b65672c
LongUnity <tranbaolong24511@gmail.com>
2024-02-03 14:26:49 +0700

    FPS_demo

---
+++ Assets/Script/FPSShooting/AnimEventHandler.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimEventHandler : MonoBehaviour
+{
+    public void SingleFireAmmoCounter()
+    {
+    }
+    public void AddSingleFireEffects()
+    {
+    }
+    public void AltFireToIdle()
+    {
+    }
+    public void PlayReloadPart1Sound()
+    {
+    }
+
+    public void PlayReloadPart2Sound()
+    {
+
+    }
+
+    public void PlayReloadPart3Sound()
+    {
+
+    }
+
+    public void PlayReloadPart4Sound()
+    {
+
+    }
+
+    public void PlayReloadPart5Sound()
+    {
+
+    }
+
+    public void AddAmmo()
+    {
+
+    }
+
+    public void ReloadToIdle()
+    {
+
+    }
+    public void PlayFireSound()
+    {
+    }
+}
